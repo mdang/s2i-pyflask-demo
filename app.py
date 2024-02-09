b38b8885c7302d002e08ee03868712ee3d6bee76
Mike Dang <re.inspire@gmail.com>
2024-02-09 10:06:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo - 3.0</h1>'
+    return '<h1>Hello, World ! - Pyflask Demo - 1.0</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>3.0</b></h1>'
+    return '<h1>App version : <b>1.0</b></h1>'
 
 @app.route('/test')
 def get_test():
